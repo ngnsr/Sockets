dea7ba381f45eff49a643a741dd2a4f7250432e9
ngnsr <vladislavrisengin895@gmail.com>
2023-04-11 23:10:14 +0300

    Added sending commands to the server

--- client/src/main/java/comp/netw/client/Client.java
+++ client/src/main/java/comp/netw/client/Client.java
@@ -1,45 +1,165 @@
 package comp.netw.client;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.IOException;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
-public class Client{
-    public static void main(String[] args){
-        Socket client;
-        BufferedOutputStream os;
-        BufferedInputStream is;
+//TODO логер клієнт + сервер
+//TODO запис простих чисел у файл.
 
-        Scanner scanner = new Scanner(System.in);
+public class Client {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static Socket client;
+    private static PrintWriter out;
+    private static BufferedReader in;
 
-        try {
-            client = new Socket("localhost", 4444);
-            os = new BufferedOutputStream(client.getOutputStream());
-            is = new BufferedInputStream(client.getInputStream());
-            String line;
-            byte[] data;
-            while(true){
-                line = scanner.nextLine();
-                if(line.equals("exit")){
-                    break;
-                }
-                data = line.getBytes(StandardCharsets.UTF_8);
-                os.write(data);
-                os.flush();
+    public static void main(final String[] args) {
+        String host = readConsoleHost();
+        int port = readConsoleNumber("Enter port number.\n > ");
 
-                line = new String(is.readAllBytes(), StandardCharsets.UTF_8);
-                System.out.println(line);
+        startConnection(host, port);
+
+        int mode = chooseMode();
+
+        executeMode(mode);
+
+        closeConnection();
+    }
+
+    private static void executeMode(int mode){
+        switch(mode){
+            case 1: requestCheckIfPrime();
+            break;
+            case 2: requestGenerateAndCheck();
+            break;
+            case 3: requestWho();
+        }
+    }
+
+    private static void requestWho() {
+        String command = "Who";
+        int commandLength = command.length();
+        out.print(commandLength);
+        out.println(command);
+
+        printResponse();
+        // logger
+    }
+
+    private static void requestGenerateAndCheck() {
+        String command = "GenerateAndCheck";
+        int commandLength = command.length();
+        out.print(commandLength);
+        out.println(command);
+        printResponse();
+        // write prime to file.
+    }
+
+    private static void requestCheckIfPrime() {
+        String command = "Check";
+        int commandLength = command.length();
+
+        final String message = "Enter number: \n > ";
+
+        String line;
+        boolean open = true;
+        while (open) {
+            System.out.println(message);
+            line = scanner.nextLine();
+            if (line.equals("exit")) {
+                break;
             }
+            if(line.matches("[+]?\\d+")){
+                out.print(commandLength);
+                out.print(command);
+                out.println(Integer.parseInt(line));
+                // write to file.
+                open = printResponse();
+            }
+        }
+    }
 
+    private static boolean printResponse(){
+        StringBuilder sb = new StringBuilder(260);
+        String line = "";
+        int testConnection = 0;
+        
+        try {
+            while((line = in.readLine()) != null){
+                sb.append(line);
+                sb.append("\n");
+            }
+            System.out.println(sb);
+            testConnection = in.read();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return testConnection >= 0 ? true : false;
+    }
+
+    private static int chooseMode() {
+        String message = "Choose mode number:\n1 - request to check whether the number is prime\n2 - generate and check 50 random numbers\n3 - Who\n > ";
+        int mode;
+
+        do {
+            mode = readConsoleNumber(message);
+        } while (mode >=1 && mode <= 3);
+        return mode;
+    }
+
+    private static void startConnection(final String host, final int port) {
+        try {
+            client = new Socket(host, port);
+            out = new PrintWriter(client.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void closeConnection() {
+        try {
             scanner.close();
-            os.close();
+            out.close();
             client.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
 
+    private static int readConsoleNumber(String message) {
+        int n = -1;
+        do {
+            System.out.print(message);
+            try {
+                n = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Try again!");
+            }
+            System.out.println();
+        } while (n < 0);
+
+        return n;
+    }
+
+    private static String readConsoleHost() {
+        String host = "";
+        String message = "Enter IPV4 host: ";
+
+        do {
+            System.out.print(message);
+            try {
+                host = scanner.nextLine().trim();
+            } catch (Exception e) {
+                System.out.println("Try again!");
+            }
+            System.out.println();
+        } while (!host.matches("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$") || !host.equals("localhost"));
+
+        return host;
     }
 }
