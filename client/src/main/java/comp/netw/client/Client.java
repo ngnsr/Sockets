a986c3b9612306e880e5d03bab5b2236b1917216
ngnsr <vladislavrisengin895@gmail.com>
2023-05-10 13:59:31 +0300

    Init

--- client/src/main/java/comp/netw/client/Client.java
+++ client/src/main/java/comp/netw/client/Client.java
@@ -1,165 +1,304 @@
 package comp.netw.client;
 
-import java.io.IOException;
-
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
-//TODO логер клієнт + сервер
-//TODO запис простих чисел у файл.
-
 public class Client {
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner consoleScanner = new Scanner(System.in);
+
     private static Socket client;
     private static PrintWriter out;
     private static BufferedReader in;
 
+    private static PrintWriter logger;
+    private static PrintWriter primesLogger;
+    private static DateTimeFormatter date;
+
+    private static InetAddress LOCAL_ADDRESS;
+    private static String HOST_ADDRESS;
+
+    private static final int CONNECT_TIMEOUT = 3000;
+
+    // if not exist create
+    private static final String LOG_PATH = System.getProperty("user.dir") + "/logs";
+    private static final String CLIENT_LOG_PATH = LOG_PATH + "/client.log";
+    private static final String PRIMES_LOG_PATH = LOG_PATH + "/primes.log";
+
+    final static String NUMBER_IS_PRIME_MSG = "is a prime";
+    final static String MODE_MSG = "Choose mode :\n1 - request to check whether the number is prime\n2 - generate and check 50 random numbers\n3 - Who\n- exit\n > ";
+
+    // command(command length)
+    enum Commands {
+        Check(5),
+        GenerateAndCheck(16),
+        Who(3);
+
+        private int length;
+
+        private Commands(int length) {
+            this.length = length;
+        }
+
+        public int getLength() {
+            return length;
+        }
+    }
+
     public static void main(final String[] args) {
-        String host = readConsoleHost();
-        int port = readConsoleNumber("Enter port number.\n > ");
+        initLoggers();
 
-        startConnection(host, port);
+        initAndStartSocketConnection();
 
-        int mode = chooseMode();
-
-        executeMode(mode);
+        while (true) {
+            final int mode = (int) chooseMode();
+            if (mode == -1 ) {
+                // request for stat
+                out.println("6 exit");
+                printResponse();
+                break;
+            }
+            makeRequest(mode);
+        }
 
         closeConnection();
     }
 
-    private static void executeMode(int mode){
-        switch(mode){
-            case 1: requestCheckIfPrime();
-            break;
-            case 2: requestGenerateAndCheck();
-            break;
-            case 3: requestWho();
+    public static void initLoggers() {
+        ensureLogsDirExists();
+
+        logger = initLogger(PRIMES_LOG_PATH);
+
+        primesLogger = initLogger(CLIENT_LOG_PATH);
+        // need for client logger
+        date = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+    }
+
+    public static void ensureLogsDirExists(){
+        final File logFolder = new File(LOG_PATH);
+        if(!logFolder.exists()){
+            try {
+                logFolder.mkdir();
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static PrintWriter initLogger(final String path) {
+        try {
+            final File file = new File(path);
+            ensureFileExists(file);
+            return new PrintWriter(new FileWriter(file, true), true);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void ensureFileExists(final File file){
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void makeRequest(final int mode) {
+        if (mode == 1) {
+            requestCheckIfPrime();
+        } else if (mode == 2) {
+            requestGenerateAndCheck();
+        } else if (mode == 3) {
+            requestWho();
         }
     }
 
     private static void requestWho() {
-        String command = "Who";
-        int commandLength = command.length();
-        out.print(commandLength);
-        out.println(command);
+        System.out.println("Who:");
+        out.println(Commands.Who.getLength() + 1 + " " + Commands.Who.name());
 
+        log(Commands.Who.name());
         printResponse();
-        // logger
     }
 
     private static void requestGenerateAndCheck() {
-        String command = "GenerateAndCheck";
-        int commandLength = command.length();
-        out.print(commandLength);
-        out.println(command);
-        printResponse();
-        // write prime to file.
-    }
+        System.out.println("Generate 50 numbers and check if they are prime:");
+        out.println(Commands.GenerateAndCheck.getLength() + 1 + " " + Commands.GenerateAndCheck.name());
+        log(Commands.GenerateAndCheck.name());
+        String inpString;
+
+        try {
+            do {
+                inpString = in.readLine();
+                sb.append(inpString + "\n");
+                logPrimeNumber(inpString);
+            } while (in.ready());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(sb.toString());
+        sb.setLength(0);
+   }
 
     private static void requestCheckIfPrime() {
-        String command = "Check";
-        int commandLength = command.length();
-
+        System.out.println("Check: ");
+        System.out.println("type \'exit\' to exit");
         final String message = "Enter number: \n > ";
 
-        String line;
-        boolean open = true;
-        while (open) {
-            System.out.println(message);
-            line = scanner.nextLine();
-            if (line.equals("exit")) {
+        String stringNumber;
+        while (true) {
+            System.out.print(message);
+            stringNumber = consoleScanner.nextLine();
+            System.out.println();
+
+            if (stringNumber.equals("exit")) {
                 break;
             }
-            if(line.matches("[+]?\\d+")){
-                out.print(commandLength);
-                out.print(command);
-                out.println(Integer.parseInt(line));
-                // write to file.
-                open = printResponse();
+
+            if (isNumberValid(stringNumber)) {
+                // out.printf("%s %s %s\n", Commands.Check.getLength(), Commands.Check.name(), stringNumber);
+                out.println(Commands.Check.getLength() + 2 + stringNumber.length() + " " + Commands.Check.name() + " " + stringNumber);
+                log(Commands.Check.name());
+                try {
+                    String response = in.readLine();
+                    System.out.println(response);
+                    logPrimeNumber(response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println();
             }
         }
     }
 
-    private static boolean printResponse(){
-        StringBuilder sb = new StringBuilder(260);
-        String line = "";
-        int testConnection = 0;
-        
+    private static void logPrimeNumber(String in) {
+        if (in.matches("^\\d+ .*") && in.endsWith(NUMBER_IS_PRIME_MSG)) {
+            primesLogger.println(in.split(" ")[0]);
+        }
+    }
+
+    private static boolean isNumberValid(final String stringNumber) {
+        boolean valid = true;
         try {
-            while((line = in.readLine()) != null){
-                sb.append(line);
-                sb.append("\n");
-            }
-            System.out.println(sb);
-            testConnection = in.read();
-        } catch (IOException e) {
+            Long.parseLong(stringNumber);
+        } catch (final NumberFormatException e) {
+            valid = false;
+        }
+        return valid;
+    }
+
+    static StringBuilder sb = new StringBuilder(1500);
+    private static void printResponse() {
+        try {
+            do {
+                sb.append(in.readLine() + "\n");
+            } while (in.ready());
+        } catch (final IOException e) {
             System.out.println(e.getMessage());
         }
-        return testConnection >= 0 ? true : false;
+        System.out.println(sb.toString());
+        sb.setLength(0);
     }
 
     private static int chooseMode() {
-        String message = "Choose mode number:\n1 - request to check whether the number is prime\n2 - generate and check 50 random numbers\n3 - Who\n > ";
-        int mode;
-
-        do {
-            mode = readConsoleNumber(message);
-        } while (mode >=1 && mode <= 3);
-        return mode;
+        String in = "";
+        int n = -1;
+        while(n < 1 || n > 3){
+            System.out.print(MODE_MSG);
+            in = consoleScanner.nextLine();
+            System.out.println();
+            if(in.equals("exit")){
+                break;
+            }
+            try {
+                n = Integer.parseInt(in);
+            } catch (final Exception e) {
+                System.out.println("Try again!");
+            }
+        } 
+        return n;
     }
 
-    private static void startConnection(final String host, final int port) {
-        try {
-            client = new Socket(host, port);
-            out = new PrintWriter(client.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
+    private static void initAndStartSocketConnection() {
+        boolean conected = false;
+        client = new Socket();
+        while (!conected) {
+            try {
+                final String host = readConsoleHost();
+                final int port = (int) readConsoleNumber("Enter port number.\n > ", 1025, 65_535);
+                final SocketAddress socketAddress = new InetSocketAddress(host, port);
+                client.connect(socketAddress, CONNECT_TIMEOUT);
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                HOST_ADDRESS = client.getInetAddress().getHostAddress();
+                LOCAL_ADDRESS = client.getLocalAddress();
+                conected = true;
+            } catch (final IOException e) {
+                conected = false;
+                System.err.println(e.getMessage());
+            }
         }
     }
 
     private static void closeConnection() {
         try {
-            scanner.close();
+            consoleScanner.close();
             out.close();
             client.close();
-        } catch (IOException e) {
+        } catch (final IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    private static int readConsoleNumber(String message) {
-        int n = -1;
+    private static long readConsoleNumber(final String message, final long MIN_VALUE, final long MAX_VALUE) {
+        long n = -1;
         do {
             System.out.print(message);
             try {
-                n = scanner.nextInt();
-            } catch (Exception e) {
+                n = Long.parseLong(consoleScanner.nextLine());
+            } catch (final Exception e) {
                 System.out.println("Try again!");
             }
             System.out.println();
-        } while (n < 0);
+        } while (n < MIN_VALUE || n > MAX_VALUE);
 
         return n;
     }
 
     private static String readConsoleHost() {
+        final String message = "Enter IPV4 host: ";
         String host = "";
-        String message = "Enter IPV4 host: ";
 
         do {
             System.out.print(message);
             try {
-                host = scanner.nextLine().trim();
-            } catch (Exception e) {
+                host = consoleScanner.nextLine().trim();
+            } catch (final Exception e) {
                 System.out.println("Try again!");
             }
             System.out.println();
-        } while (!host.matches("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$") || !host.equals("localhost"));
+        } while (!host.matches("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$") && !host.equals("localhost"));
 
         return host;
+    }
+
+    private static void log(String command) {
+        logger.printf("[ %s | %s | %s ] : %s\n", date.format(LocalDateTime.now()), LOCAL_ADDRESS, HOST_ADDRESS,
+                command);
     }
 }
--- server/src/main/java/comp/netw/server/Server.java
+++ server/src/main/java/comp/netw/server/Server.java
@@ -1,39 +1,223 @@
 package comp.netw.server;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 public class Server {
 
-    public static void main(String[] args) {
-        ServerSocket serverSocket;
-        Socket socket;
-        BufferedInputStream is;
-        BufferedOutputStream os;
+    final static String WHO_MSG = "Author: Risenhin Vladyslav\nVariant: 28\nDescription: Prime numbers filter";
 
-        try {
-            serverSocket = new ServerSocket(4444);
-            socket = serverSocket.accept();
-            String line;
-            while (true) {
-                is = new BufferedInputStream(socket.getInputStream());
-                os = new BufferedOutputStream(socket.getOutputStream());
-                line = new String(is.readAllBytes(), StandardCharsets.UTF_8);
-                if(line.equals("exit") || line.isBlank()){
-                    break;
-                }
-                os.write(line.getBytes(StandardCharsets.UTF_8));
-            }
-            is.close();
-            socket.close();
-            serverSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static final int PORT = 4444;
+
+    private static final String LOG_PATH = System.getProperty("user.dir") + "/logs";
+    private static final String SERVER_LOG_PATH = LOG_PATH + "/server.log";
+    private static final Random random = new Random();
+
+    private static ServerSocket serverSocket;
+    private static DateTimeFormatter date;
+    private static PrintWriter logger;
+
+    final static String NUMBER_IS_PRIME_MSG = "is a prime";
+    final static String NUMBER_IN_NOT_PRIME_MSG = "is not a prime";
+
+    // command(command length)
+    enum Commands {
+        Check(5),
+        GenerateAndCheck(16),
+        Who(3);
+
+        private int length;
+
+        private Commands(int length) {
+            this.length = length;
         }
 
+        public int getLength() {
+            return length;
+        }
+    }
+
+    public static void main(final String[] args) {
+        initLogger();
+
+        initServerSocket();
+
+        while (true) {
+            try {
+                new ClientHandler(serverSocket.accept()).start();
+            } catch (final IOException e) {
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void ensureLogsDirExists(){
+        final File logFolder = new File(LOG_PATH);
+        if(!logFolder.exists()){
+            try {
+                logFolder.mkdir();
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void initLogger() {
+        date = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+
+        try {
+            final File file = new File(SERVER_LOG_PATH);
+
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            logger = new PrintWriter(new FileWriter(file, true), true);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void initServerSocket() {
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (final IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    private static class ClientHandler extends Thread {
+
+        private final Socket clientSocket;
+        private InetAddress LOCAL_ADDRESS;
+        private String HOST_ADDRESS;
+
+        // stat
+        private int count = 0;
+        private int primeCount = 0;
+        private long min = Integer.MAX_VALUE;
+        private long max = Integer.MIN_VALUE;
+
+        public ClientHandler(final Socket clientSocket) {
+            this.clientSocket = clientSocket;
+            HOST_ADDRESS = clientSocket.getInetAddress().getHostAddress();
+            LOCAL_ADDRESS = clientSocket.getLocalAddress();
+        }
+
+        @Override
+        public void run() {
+            String request = "";
+            try (
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+                while ((request = in.readLine()) != null) {
+                    executeRuquest(request.trim(), out, in);
+                }
+            } catch (final Exception e) {
+            }
+        }
+
+        private void executeRuquest(final String request, final PrintWriter out, final BufferedReader in) {
+            int requestLength = -1;
+            String command = "";
+            long number = -1;
+
+            if (request.endsWith("exit")) {
+                out.printf("Total numbers: %s\nPrime numbers: %s\nMax number: %s\nMin number: %s\n", count, primeCount,
+                        count > 0 ? max : "-", count > 0 ? min : "-");
+            } else if (request.matches("\\d+\\s+[a-zA-Z]+(\\s+[+-]?\\d+)?")) {
+                final String[] args = request.split("\\s+");
+
+                // int too large
+                requestLength = Integer.parseInt(args[0]);
+                command = args[1];
+
+                // validate command and exec
+                if (args.length == 2) {
+                    if (command.equalsIgnoreCase(Commands.Who.name())) {
+                        executeCommandWho(out);
+                    } else if (command.equalsIgnoreCase(Commands.GenerateAndCheck.name())) {
+                        executeCommandGenerateAndCheck(out);
+                    }
+                } else if (args.length == 3 && command.equalsIgnoreCase(Commands.Check.name())) {
+                    number = Long.parseLong(args[2]);
+                    executeCommandCheck(out, number);
+                }
+            } else {
+                out.println("Wrong request!");
+            }
+        }
+
+        private void executeCommandWho(final PrintWriter out) {
+            out.println(WHO_MSG);
+            log(Commands.Who.name());
+        }
+
+        private void executeCommandGenerateAndCheck(final PrintWriter out) {
+            StringBuilder sb = new StringBuilder(1300);
+            for (int i = 0; i < 50; i++) {
+                long n = Math.abs(Server.random.nextInt());
+                boolean isPrime = isPrime(n);
+                updateStat(n, isPrime);
+                sb.append(String.format("%s %s\n", n, isPrime ? NUMBER_IS_PRIME_MSG : NUMBER_IN_NOT_PRIME_MSG));
+            }
+
+            out.printf(sb.toString());
+            log(Commands.GenerateAndCheck.name());
+        }
+
+        private void executeCommandCheck(final PrintWriter out, final long number) {
+            final boolean isPrime = isPrime(number);
+
+            out.printf("%s %s\n", number, isPrime ? NUMBER_IS_PRIME_MSG : NUMBER_IN_NOT_PRIME_MSG);
+
+            updateStat(number, isPrime);
+            log(Commands.Check.name());
+        }
+
+        private void updateStat(long number, boolean isPrime) {
+            count++;
+            if (number > max) {
+                max = number;
+            }
+            if (number < min) {
+                min = number;
+            }
+            if (isPrime) {
+                primeCount++;
+            }
+        }
+
+        private boolean isPrime(final long n) {
+            if (n <= 0 || n == 1) {
+                return false;
+            }
+            for (long i = 2; i < (long) Math.sqrt(n) + 1; i++) {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        private void log(String command) {
+            logger.printf("[ %s | %s | %s ] : %s\n", date.format(LocalDateTime.now()), LOCAL_ADDRESS, HOST_ADDRESS,
+                    command);
+        }
     }
 }
